9a34a4567b50882c091f3278ef135d9314d494d6
Seyed Mojtaba Hosseini Zeidabadi <hosseini@chenzook.com>
2019-06-13 02:38:43 +0430

    Initial Commit

---
+++ SUI/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  SUI
+//
+//  Created by Seyed Mojtaba Hosseini Zeidabadi on 6/13/19.
+//  Copyright © 2019 Seyed Mojtaba Hosseini Zeidabadi. All rights reserved.
+//
+//  Linkedin: https://linkedin.com/in/MojtabaHosseini
+//  GitHub: https://github.com/MojtabaHs
+//  Web: https://chenzook.com
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
